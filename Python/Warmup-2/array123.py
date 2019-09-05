8fd4763070894a8306bbc528c075d85a97095748
Le Aundre Jackson <dell.5287@gmail.com>
2019-09-05 09:24:05 -0400

    Update file to some True and False

--- Python/Warmup-2/array123.py
+++ Python/Warmup-2/array123.py
@@ -6,8 +6,14 @@ array123([1, 1, 2, 4, 1]) → False
 array123([1, 1, 2, 1, 2, 3]) → True
 """
 
+
 def array123(nums):
-    for i in range(len(nums)-2):
-        if nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3:
-            return True
-    return False
+    for i in range(len(nums) - 2):
+        if nums[i] == 1 and nums[i + 1] == 2 and nums[i + 2] == 3:
+            return print("True")
+    return print("False")
+
+
+array123([1, 1, 2, 3, 1])
+array123([1, 1, 2, 4, 1])
+array123([1, 1, 2, 1, 2, 3])
